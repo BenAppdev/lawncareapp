bd793a1d5414048a8dbf032d235ebcd7213e2bad
benjamin <benjamin@AMDTR4>
2021-08-06 11:42:21 -0400

    Add project files.

---
+++ lawncareapp/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace lawncareapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_yes = new System.Windows.Forms.Button();
+            this.lbl_grass_question = new System.Windows.Forms.Label();
+            this.btn_no = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btn_no_2 = new System.Windows.Forms.Button();
+            this.lbl_water_question = new System.Windows.Forms.Label();
+            this.btn_yes_2 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btn_no_3 = new System.Windows.Forms.Button();
+            this.lbl_weed_question = new System.Windows.Forms.Label();
+            this.btn_yes_3 = new System.Windows.Forms.Button();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.gbx1 = new System.Windows.Forms.GroupBox();
+            this.rtxbx1 = new System.Windows.Forms.RichTextBox();
+            this.rtxbx2 = new System.Windows.Forms.RichTextBox();
+            this.rtxbx3 = new System.Windows.Forms.RichTextBox();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gbx1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_yes
+            // 
+            this.btn_yes.Location = new System.Drawing.Point(304, 132);
+            this.btn_yes.Name = "btn_yes";
+            this.btn_yes.Size = new System.Drawing.Size(71, 31);
+            this.btn_yes.TabIndex = 0;
+            this.btn_yes.Text = "Yes";
+            this.btn_yes.UseVisualStyleBackColor = true;
+            this.btn_yes.Click += new System.EventHandler(this.btn_yes_Click);
+            // 
+            // lbl_grass_question
+            // 
+            this.lbl_grass_question.AutoSize = true;
+            this.lbl_grass_question.Location = new System.Drawing.Point(81, 141);
+            this.lbl_grass_question.Name = "lbl_grass_question";
+            this.lbl_grass_question.Size = new System.Drawing.Size(186, 13);
+            this.lbl_grass_question.TabIndex = 1;
+            this.lbl_grass_question.Text = "Hey did you cut your grass this week?";
+            // 
+            // btn_no
+            // 
+            this.btn_no.Location = new System.Drawing.Point(381, 132);
+            this.btn_no.Name = "btn_no";
+            this.btn_no.Size = new System.Drawing.Size(71, 31);
+            this.btn_no.TabIndex = 2;
+            this.btn_no.Text = "No";
+            this.btn_no.UseVisualStyleBackColor = true;
+            this.btn_no.Click += new System.EventHandler(this.btn_no_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(81, 173);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(23, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "lbl2";
+            // 
+            // btn_no_2
+            // 
+            this.btn_no_2.Location = new System.Drawing.Point(381, 201);
+            this.btn_no_2.Name = "btn_no_2";
+            this.btn_no_2.Size = new System.Drawing.Size(71, 31);
+            this.btn_no_2.TabIndex = 6;
+            this.btn_no_2.Text = "No";
+            this.btn_no_2.UseVisualStyleBackColor = true;
+            this.btn_no_2.Click += new System.EventHandler(this.btn_no_2_Click);
+            // 
+            // lbl_water_question
+            // 
+            this.lbl_water_question.AutoSize = true;
+            this.lbl_water_question.Location = new System.Drawing.Point(81, 210);
+            this.lbl_water_question.Name = "lbl_water_question";
+            this.lbl_water_question.Size = new System.Drawing.Size(199, 13);
+            this.lbl_water_question.TabIndex = 5;
+            this.lbl_water_question.Text = "Have you watered your grass this week?";
+            this.lbl_water_question.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_yes_2
+            // 
+            this.btn_yes_2.Location = new System.Drawing.Point(304, 201);
+            this.btn_yes_2.Name = "btn_yes_2";
+            this.btn_yes_2.Size = new System.Drawing.Size(71, 31);
+            this.btn_yes_2.TabIndex = 4;
+            this.btn_yes_2.Text = "Yes";
+            this.btn_yes_2.UseVisualStyleBackColor = true;
+            this.btn_yes_2.Click += new System.EventHandler(this.btn_yes_2_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(81, 244);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(23, 13);
+            this.lbl3.TabIndex = 7;
+            this.lbl3.Text = "lbl3";
+            // 
+            // btn_no_3
+            // 
+            this.btn_no_3.Location = new System.Drawing.Point(381, 271);
+            this.btn_no_3.Name = "btn_no_3";
+            this.btn_no_3.Size = new System.Drawing.Size(71, 31);
+            this.btn_no_3.TabIndex = 10;
+            this.btn_no_3.Text = "No";
+            this.btn_no_3.UseVisualStyleBackColor = true;
+            this.btn_no_3.Click += new System.EventHandler(this.btn_no_3_Click);
+            // 
+            // lbl_weed_question
+            // 
+            this.lbl_weed_question.AutoSize = true;
+            this.lbl_weed_question.Location = new System.Drawing.Point(81, 280);
+            this.lbl_weed_question.Name = "lbl_weed_question";
+            this.lbl_weed_question.Size = new System.Drawing.Size(141, 13);
+            this.lbl_weed_question.TabIndex = 9;
+            this.lbl_weed_question.Text = "Did you add the weed killer?";
+            // 
+            // btn_yes_3
+            // 
+            this.btn_yes_3.Location = new System.Drawing.Point(304, 271);
+            this.btn_yes_3.Name = "btn_yes_3";
+            this.btn_yes_3.Size = new System.Drawing.Size(71, 31);
+            this.btn_yes_3.TabIndex = 8;
+            this.btn_yes_3.Text = "Yes";
+            this.btn_yes_3.UseVisualStyleBackColor = true;
+            this.btn_yes_3.Click += new System.EventHandler(this.btn_yes_3_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(81, 324);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(23, 13);
+            this.lbl4.TabIndex = 11;
+            this.lbl4.Text = "lbl4";
+            // 
+            // gbx1
+            // 
+            this.gbx1.Controls.Add(this.rtxbx3);
+            this.gbx1.Controls.Add(this.rtxbx2);
+            this.gbx1.Controls.Add(this.rtxbx1);
+            this.gbx1.Location = new System.Drawing.Point(35, 376);
+            this.gbx1.Name = "gbx1";
+            this.gbx1.Size = new System.Drawing.Size(445, 198);
+            this.gbx1.TabIndex = 12;
+            this.gbx1.TabStop = false;
+            this.gbx1.Text = "Recommendations";
+            // 
+            // rtxbx1
+            // 
+            this.rtxbx1.Location = new System.Drawing.Point(6, 19);
+            this.rtxbx1.Name = "rtxbx1";
+            this.rtxbx1.Size = new System.Drawing.Size(417, 51);
+            this.rtxbx1.TabIndex = 1;
+            this.rtxbx1.Text = "Experts say: Cutting your grass Once a week allows for grasses to grow more beaut" +
+    "iful";
+            this.rtxbx1.Visible = false;
+            this.rtxbx1.TextChanged += new System.EventHandler(this.rtxbx1_TextChanged);
+            // 
+            // rtxbx2
+            // 
+            this.rtxbx2.Location = new System.Drawing.Point(6, 76);
+            this.rtxbx2.Name = "rtxbx2";
+            this.rtxbx2.Size = new System.Drawing.Size(417, 51);
+            this.rtxbx2.TabIndex = 2;
+            this.rtxbx2.Text = "Experts say: Watering your grass regulalry gives the growing grasses more nutrien" +
+    "t water that helps the grass grow well";
+            this.rtxbx2.Visible = false;
+            this.rtxbx2.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // rtxbx3
+            // 
+            this.rtxbx3.Location = new System.Drawing.Point(6, 133);
+            this.rtxbx3.Name = "rtxbx3";
+            this.rtxbx3.Size = new System.Drawing.Size(417, 51);
+            this.rtxbx3.TabIndex = 3;
+            this.rtxbx3.Text = "Experts say: add weed terminator allows the grass to grow without having to share" +
+    " space with a weed. This means it will grow faster and easier all year long";
+            this.rtxbx3.Visible = false;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(203, 593);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 13;
+            this.btn_reset.Text = "Reset Form";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::lawncareapp.Properties.Resources.grp_title;
+            this.pictureBox1.ErrorImage = global::lawncareapp.Properties.Resources.grp_title;
+            this.pictureBox1.InitialImage = global::lawncareapp.Properties.Resources.grp_title;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 48);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 665);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.gbx1);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.btn_no_3);
+            this.Controls.Add(this.lbl_weed_question);
+            this.Controls.Add(this.btn_yes_3);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.btn_no_2);
+            this.Controls.Add(this.lbl_water_question);
+            this.Controls.Add(this.btn_yes_2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btn_no);
+            this.Controls.Add(this.lbl_grass_question);
+            this.Controls.Add(this.btn_yes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbx1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_yes;
+        private System.Windows.Forms.Label lbl_grass_question;
+        private System.Windows.Forms.Button btn_no;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btn_no_2;
+        private System.Windows.Forms.Label lbl_water_question;
+        private System.Windows.Forms.Button btn_yes_2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btn_no_3;
+        private System.Windows.Forms.Label lbl_weed_question;
+        private System.Windows.Forms.Button btn_yes_3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.GroupBox gbx1;
+        private System.Windows.Forms.RichTextBox rtxbx1;
+        private System.Windows.Forms.RichTextBox rtxbx3;
+        private System.Windows.Forms.RichTextBox rtxbx2;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ lawncareapp/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lawncareapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl2.Visible = false;
+            lbl3.Visible = false;
+            lbl4.Visible = false;
+        }
+
+
+
+        private void btn_yes_Click(object sender, EventArgs e)
+        {
+            lbl2.Visible = true;
+            lbl2.Text = "Great Job see you next month";
+            lbl2.ForeColor = Color.Black;
+            rtxbx1.Visible = true;
+        }
+
+        private void btn_no_Click(object sender, EventArgs e)
+        {
+            lbl2.Visible = true;
+            lbl2.Text = "All right now its time to get on it";
+            lbl2.ForeColor = Color.Red;
+            rtxbx1.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_yes_2_Click(object sender, EventArgs e)
+        {
+            lbl3.Visible = true;
+            lbl3.Text = "How refreshing";
+            lbl3.ForeColor = Color.Black;
+            rtxbx2.Visible = true;
+        }
+
+        private void btn_no_2_Click(object sender, EventArgs e)
+        {
+            lbl3.Visible = true;
+            lbl3.Text = "Looks like someone is thirsty";
+            lbl3.ForeColor = Color.Red;
+            rtxbx2.Visible = true;
+        }
+
+        private void btn_yes_3_Click(object sender, EventArgs e)
+        {
+            lbl4.Visible = true;
+            lbl4.Text = "Great your grass is happy not being crowded";
+            lbl4.ForeColor = Color.Black;
+            rtxbx3.Visible = true;
+        }
+
+        private void btn_no_3_Click(object sender, EventArgs e)
+        {
+            lbl4.Visible = true;
+            lbl4.Text = "Oh man its too crowded, weeds and grass together";
+            lbl4.ForeColor = Color.Red;
+            rtxbx3.Visible = true;
+
+        }
+
+        private void rtxbx1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            rtxbx1.Visible = false;
+            rtxbx2.Visible = false;
+            rtxbx3.Visible = false;
+
+            lbl2.Visible = false;
+            lbl3.Visible = false;
+            lbl4.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
